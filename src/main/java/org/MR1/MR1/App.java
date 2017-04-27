35f94d774552ca42e2941f9efa6bf02ddc01634e
Nivedha <nivedha.v@mstsolutions.com>
2017-04-27 13:18:45 +0530

    Commit

---
+++ src/main/java/org/MR1/MR1/App.java
@@ -0,0 +1,13 @@
+package org.MR1.MR1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
